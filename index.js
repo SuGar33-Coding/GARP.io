aeb4d5ba00924c2bec9d552787d5d5f9ffa7c17e
Gabriel Klavans <gabeklav@bu.edu>
2019-10-14 00:30:53 -0400

    Proper tabbing

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ const { JSDOM } = jsdom;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 function setupAuthoritativePhaser() {
-  JSDOM.fromFile(path.join(__dirname, 'server/index.html'), {
-    // To run the scripts in the html file
-    runScripts: "dangerously",
-    // Also load supported external resources
-    resources: "usable",
-    // So requestAnimatinFrame events fire
-    pretendToBeVisual: true
-  }).then((dom) => {
-    dom.window.URL.createObjectURL = (blob) => {
-      if (blob){
-        return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
-      }
-    };
-    dom.window.URL.revokeObjectURL = (objectURL) => {};
-    dom.window.gameLoaded = () => {
-      server.listen(8082, function () {
-        console.log(`Listening on ${server.address().port}`);
-      });
-    };
-    dom.window.io = io;
-  }).catch((error) => {
-    console.log(error.message);
-  });
+    JSDOM.fromFile(path.join(__dirname, 'server/index.html'), {
+        // To run the scripts in the html file
+        runScripts: "dangerously",
+        // Also load supported external resources
+        resources: "usable",
+        // So requestAnimatinFrame events fire
+        pretendToBeVisual: true
+    }).then((dom) => {
+        dom.window.URL.createObjectURL = (blob) => {
+            if (blob) {
+                return datauri.format(blob.type, blob[Object.getOwnPropertySymbols(blob)[0]]._buffer).content;
+            }
+        };
+        dom.window.URL.revokeObjectURL = (objectURL) => { };
+        dom.window.gameLoaded = () => {
+            server.listen(8082, function () {
+                console.log(`Listening on ${server.address().port}`);
+            });
+        };
+        dom.window.io = io;
+    }).catch((error) => {
+        console.log(error.message);
+    });
 }
 
 setupAuthoritativePhaser();
--- public/js/game.js
+++ public/js/game.js
@@ -1,112 +1,112 @@
 var config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('ship', 'assets/spaceShips_001.png');
-  this.load.image('otherPlayer', 'assets/enemyBlack5.png');
-  this.load.image('star', 'assets/star_gold.png');
+    this.load.image('ship', 'assets/spaceShips_001.png');
+    this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+    this.load.image('star', 'assets/star_gold.png');
 }
 
 function create() {
-  var self = this;
-  this.socket = io();
-  this.players = this.add.group();
+    var self = this;
+    this.socket = io();
+    this.players = this.add.group();
 
-  this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });
-  this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
+    this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });
+    this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#FF0000' });
 
-  this.socket.on('currentPlayers', function (players) {
-    Object.keys(players).forEach(function (id) {
-      if (players[id].playerId === self.socket.id) {
-        displayPlayers(self, players[id], 'ship');
-      } else {
-        displayPlayers(self, players[id], 'otherPlayer');
-      }
+    this.socket.on('currentPlayers', function (players) {
+        Object.keys(players).forEach(function (id) {
+            if (players[id].playerId === self.socket.id) {
+                displayPlayers(self, players[id], 'ship');
+            } else {
+                displayPlayers(self, players[id], 'otherPlayer');
+            }
+        });
     });
-  });
 
-  this.socket.on('newPlayer', function (playerInfo) {
-    displayPlayers(self, playerInfo, 'otherPlayer');
-  });
-
-  this.socket.on('disconnect', function (playerId) {
-    self.players.getChildren().forEach(function (player) {
-      if (playerId === player.playerId) {
-        player.destroy();
-      }
+    this.socket.on('newPlayer', function (playerInfo) {
+        displayPlayers(self, playerInfo, 'otherPlayer');
     });
-  });
 
-  this.socket.on('playerUpdates', function (players) {
-    Object.keys(players).forEach(function (id) {
-      self.players.getChildren().forEach(function (player) {
-        if (players[id].playerId === player.playerId) {
-          player.setRotation(players[id].rotation);
-          player.setPosition(players[id].x, players[id].y);
+    this.socket.on('disconnect', function (playerId) {
+        self.players.getChildren().forEach(function (player) {
+            if (playerId === player.playerId) {
+                player.destroy();
+            }
+        });
+    });
+
+    this.socket.on('playerUpdates', function (players) {
+        Object.keys(players).forEach(function (id) {
+            self.players.getChildren().forEach(function (player) {
+                if (players[id].playerId === player.playerId) {
+                    player.setRotation(players[id].rotation);
+                    player.setPosition(players[id].x, players[id].y);
+                }
+            });
+        });
+    });
+
+    this.socket.on('updateScore', function (scores) {
+        self.blueScoreText.setText('Blue: ' + scores.blue);
+        self.redScoreText.setText('Red: ' + scores.red);
+    });
+
+    this.socket.on('starLocation', function (starLocation) {
+        if (!self.star) {
+            self.star = self.add.image(starLocation.x, starLocation.y, 'star');
+        } else {
+            self.star.setPosition(starLocation.x, starLocation.y);
         }
-      });
     });
-  });
 
-  this.socket.on('updateScore', function (scores) {
-    self.blueScoreText.setText('Blue: ' + scores.blue);
-    self.redScoreText.setText('Red: ' + scores.red);
-  });
-
-  this.socket.on('starLocation', function (starLocation) {
-    if (!self.star) {
-      self.star = self.add.image(starLocation.x, starLocation.y, 'star');
-    } else {
-      self.star.setPosition(starLocation.x, starLocation.y);
-    }
-  });
-
-  this.cursors = this.input.keyboard.createCursorKeys();
-  this.leftKeyPressed = false;
-  this.rightKeyPressed = false;
-  this.upKeyPressed = false;
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.leftKeyPressed = false;
+    this.rightKeyPressed = false;
+    this.upKeyPressed = false;
 }
 
 function update() {
-  const left = this.leftKeyPressed;
-  const right = this.rightKeyPressed;
-  const up = this.upKeyPressed;
+    const left = this.leftKeyPressed;
+    const right = this.rightKeyPressed;
+    const up = this.upKeyPressed;
 
-  if (this.cursors.left.isDown) {
-    this.leftKeyPressed = true;
-  } else if (this.cursors.right.isDown) {
-    this.rightKeyPressed = true;
-  } else {
-    this.leftKeyPressed = false;
-    this.rightKeyPressed = false;
-  }
+    if (this.cursors.left.isDown) {
+        this.leftKeyPressed = true;
+    } else if (this.cursors.right.isDown) {
+        this.rightKeyPressed = true;
+    } else {
+        this.leftKeyPressed = false;
+        this.rightKeyPressed = false;
+    }
 
-  if (this.cursors.up.isDown) {
-    this.upKeyPressed = true;
-  } else {
-    this.upKeyPressed = false;
-  }
+    if (this.cursors.up.isDown) {
+        this.upKeyPressed = true;
+    } else {
+        this.upKeyPressed = false;
+    }
 
-  if (left !== this.leftKeyPressed || right !== this.rightKeyPressed || up !== this.upKeyPressed) {
-    this.socket.emit('playerInput', { left: this.leftKeyPressed , right: this.rightKeyPressed, up: this.upKeyPressed });
-  }
+    if (left !== this.leftKeyPressed || right !== this.rightKeyPressed || up !== this.upKeyPressed) {
+        this.socket.emit('playerInput', { left: this.leftKeyPressed, right: this.rightKeyPressed, up: this.upKeyPressed });
+    }
 }
 
 function displayPlayers(self, playerInfo, sprite) {
-  const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-  if (playerInfo.team === 'blue') player.setTint(0x0000ff);
-  else player.setTint(0xff0000);
-  player.playerId = playerInfo.playerId;
-  self.players.add(player);
+    const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') player.setTint(0x0000ff);
+    else player.setTint(0xff0000);
+    player.playerId = playerInfo.playerId;
+    self.players.add(player);
 }
