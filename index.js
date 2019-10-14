64b7dee42da5a2b550fed6314c977e94cae401f0
Gabriel Klavans <gabeklav@bu.edu>
2019-10-14 02:21:20 -0400

    Decoupled socketio from specific phaser instance

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io').listen(server);
+const uniqid = require('uniqid');
 const Datauri = require('datauri');
 
 const datauri = new Datauri();
@@ -15,7 +16,9 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-function setupAuthoritativePhaser() {
+var instances = {};
+
+function setupAuthoritativePhaser(roomName) {
     JSDOM.fromFile(path.join(__dirname, 'server/index.html'), {
         // To run the scripts in the html file
         runScripts: "dangerously",
@@ -30,10 +33,9 @@ function setupAuthoritativePhaser() {
             }
         };
         dom.window.URL.revokeObjectURL = (objectURL) => { };
-        dom.window.gameLoaded = () => {
-            server.listen(8082, function () {
-                console.log(`Listening on ${server.address().port}`);
-            });
+        dom.window.gameLoaded = (game) => {
+            instances[roomName] = game;
+            console.log(Object.keys(instances));
         };
         dom.window.io = io;
     }).catch((error) => {
@@ -41,4 +43,69 @@ function setupAuthoritativePhaser() {
     });
 }
 
-setupAuthoritativePhaser();
+var name1 = uniqid("room-");
+setupAuthoritativePhaser(name1);
+var name2 = uniqid("room-");
+setupAuthoritativePhaser(name2);
+
+io.on('connection', function (socket) {
+    let roomName = name2;
+    let game = instances[roomName];
+    console.log('a user connected to room: ' + roomName);
+    // create a new player and add it to our clients object
+    game.clients[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
+        input: {
+            left: false,
+            right: false,
+            up: false
+        }
+    };
+    console.log("list of clients: " + Object.keys(game.clients));
+    // add player to server
+    game.addPlayer(game, socket.id);
+    // send the clients object to the new player
+    socket.emit('currentPlayers', game.clients);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', game.clients[socket.id]);
+    // send the star object to the new player
+    socket.emit('starLocation', { x: game.star.x, y: game.star.y });
+    // send the current scores
+    socket.emit('updateScore', game.scores);
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        // remove player from server
+        game.removePlayer(game, socket.id);
+        // remove this player from our players object
+        delete game.clients[socket.id];
+        // emit a message to all players to remove this player
+        io.emit('disconnect', socket.id);
+    });
+
+    // when a player moves, update the player data
+    socket.on('playerInput', function (inputData) {
+        game.handlePlayerInput(game, socket.id, inputData);
+    });
+});
+
+server.listen(8082, function () {
+    server.clientUpdateRate = 1000 / 15; // Rate at which update packets are sent
+    //server.setUpdateLoop();
+    console.log(`Listening on ${server.address().port}`);
+    console.log(`Address should be: http://localhost:${server.address().port}`);
+});
+
+server.sendUpdate = () => {
+
+    // For every dungeon, for each baddie in that dungeon, find the player that the baddie is closest to
+    // TODO: Only change this on player move rather than in sendUpdate
+    Object.keys(package.dungeons).forEach(roomName => {
+
+        io.to(roomName).emit('update', package.dungeons[roomName]);
+    });
+};
--- server/js/game.js
+++ server/js/game.js
@@ -1,4 +1,4 @@
-const players = {};
+//const players = {};
 
 const config = {
     type: Phaser.HEADLESS,
@@ -26,7 +26,34 @@ function preload() {
 }
 
 function create() {
+    this.addPlayer = function (self, playerInfo) {
+        const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+        player.setDrag(100);
+        player.setAngularDrag(100);
+        player.setMaxVelocity(200);
+        player.playerId = playerInfo;
+        self.players.add(player);
+    }
+
+    this.handlePlayerInput = function (self, playerId, input) {
+        self.players.getChildren().forEach((player) => {
+            if (playerId === player.playerId) {
+                this.clients[player.playerId].input = input;
+            }
+        });
+    }
+
+    this.removePlayer = function (self, playerId) {
+        self.players.getChildren().forEach((player) => {
+            if (playerId === player.playerId) {
+                player.destroy();
+            }
+        });
+    }
+
+    this.clients = {};
     const self = this;
+    this.message = 0;
     this.players = this.physics.add.group();
 
     this.scores = {
@@ -38,62 +65,67 @@ function create() {
     this.physics.add.collider(this.players);
 
     this.physics.add.overlap(this.players, this.star, function (star, player) {
-        if (players[player.playerId].team === 'red') {
+        if (self.clients[player.playerId].team === 'red') {
             self.scores.red += 10;
+            //console.log(self.scores.red);
         } else {
             self.scores.blue += 10;
+            //console.log(self.scores.blue);
         }
         self.star.setPosition(randomPosition(700), randomPosition(500));
         io.emit('updateScore', self.scores);
         io.emit('starLocation', { x: self.star.x, y: self.star.y });
     });
 
-    io.on('connection', function (socket) {
-        console.log('a user connected');
-        // create a new player and add it to our players object
-        players[socket.id] = {
-            rotation: 0,
-            x: Math.floor(Math.random() * 700) + 50,
-            y: Math.floor(Math.random() * 500) + 50,
-            playerId: socket.id,
-            team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
-            input: {
-                left: false,
-                right: false,
-                up: false
-            }
-        };
-        // add player to server
-        addPlayer(self, players[socket.id]);
-        // send the players object to the new player
-        socket.emit('currentPlayers', players);
-        // update all other players of the new player
-        socket.broadcast.emit('newPlayer', players[socket.id]);
-        // send the star object to the new player
-        socket.emit('starLocation', { x: self.star.x, y: self.star.y });
-        // send the current scores
-        socket.emit('updateScore', self.scores);
+    // io.on('connection', function (socket) {
+    //     console.log('a user connected');
+    //     // create a new player and add it to our players object
+    //     players[socket.id] = {
+    //         rotation: 0,
+    //         x: Math.floor(Math.random() * 700) + 50,
+    //         y: Math.floor(Math.random() * 500) + 50,
+    //         playerId: socket.id,
+    //         team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
+    //         input: {
+    //             left: false,
+    //             right: false,
+    //             up: false
+    //         }
+    //     };
+    //     // add player to server
+    //     addPlayer(self, players[socket.id]);
+    //     // send the players object to the new player
+    //     socket.emit('currentPlayers', players);
+    //     // update all other players of the new player
+    //     socket.broadcast.emit('newPlayer', players[socket.id]);
+    //     // send the star object to the new player
+    //     socket.emit('starLocation', { x: self.star.x, y: self.star.y });
+    //     // send the current scores
+    //     socket.emit('updateScore', self.scores);
 
-        socket.on('disconnect', function () {
-            console.log('user disconnected');
-            // remove player from server
-            removePlayer(self, socket.id);
-            // remove this player from our players object
-            delete players[socket.id];
-            // emit a message to all players to remove this player
-            io.emit('disconnect', socket.id);
-        });
+    //     socket.on('disconnect', function () {
+    //         console.log('user disconnected');
+    //         // remove player from server
+    //         removePlayer(self, socket.id);
+    //         // remove this player from our players object
+    //         delete players[socket.id];
+    //         // emit a message to all players to remove this player
+    //         io.emit('disconnect', socket.id);
+    //     });
 
-        // when a player moves, update the player data
-        socket.on('playerInput', function (inputData) {
-            handlePlayerInput(self, socket.id, inputData);
-        });
-    });
+    //     // when a player moves, update the player data
+    //     socket.on('playerInput', function (inputData) {
+    //         handlePlayerInput(self, socket.id, inputData);
+    //     });
+    // });
+
+    window.gameLoaded(this);
 }
 
 function update() {
+    this.message += 1;
     this.players.getChildren().forEach((player) => {
-        const input = players[player.playerId].input;
+        const input = this.clients[player.playerId].input;
         if (input.left) {
             player.setAngularVelocity(-300);
         } else if (input.right) {
@@ -108,42 +140,16 @@ function update() {
             player.setAcceleration(0);
         }
 
-        players[player.playerId].x = player.x;
-        players[player.playerId].y = player.y;
-        players[player.playerId].rotation = player.rotation;
+        this.clients[player.playerId].x = player.x;
+        this.clients[player.playerId].y = player.y;
+        this.clients[player.playerId].rotation = player.rotation;
     });
     this.physics.world.wrap(this.players, 5);
-    io.emit('playerUpdates', players);
+    io.emit('playerUpdates', this.clients);
 }
 
 function randomPosition(max) {
     return Math.floor(Math.random() * max) + 50;
 }
 
-function handlePlayerInput(self, playerId, input) {
-    self.players.getChildren().forEach((player) => {
-        if (playerId === player.playerId) {
-            players[player.playerId].input = input;
-        }
-    });
-}
-
-function addPlayer(self, playerInfo) {
-    const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-    player.setDrag(100);
-    player.setAngularDrag(100);
-    player.setMaxVelocity(200);
-    player.playerId = playerInfo.playerId;
-    self.players.add(player);
-}
-
-function removePlayer(self, playerId) {
-    self.players.getChildren().forEach((player) => {
-        if (playerId === player.playerId) {
-            player.destroy();
-        }
-    });
-}
-
 const game = new Phaser.Game(config);
-window.gameLoaded();
