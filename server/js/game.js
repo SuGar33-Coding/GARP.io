d54f42b01320c100dda425da5af3da5c325dc00d
GabeKlavans <32149200+gabeklavans@users.noreply.github.com>
2019-11-16 13:07:10 -0500

    Added assets to server dir

--- server/js/game.js
+++ server/js/game.js
@@ -22,14 +22,53 @@ const config = {
 };
 
 function preload() {
+    // Load tilemap stuff
+    this.load.spritesheet('player', 'assets/sprites/Player2.png', 38, 64);
+    this.load.spritesheet('attack', 'assets/sprites/attack.png', 16, 16);
+    this.load.spritesheet('spear', 'assets/sprites/spear.png', 5, 57);
+    this.load.spritesheet('baddie', 'assets/sprites/baddie.png');
+    this.load.spritesheet('baddieHurt', 'assets/sprites/baddieHurt.png');
+    this.load.spritesheet('chest', 'assets/sprites/chest.png');
+    this.load.spritesheet('zombie', 'assets/sprites/zombie.png');
+    this.load.spritesheet('playButton', 'assets/images/BetterPlayButton.png');
+    this.load.spritesheet('createButton', 'assets/images/createButton.png');
+    this.load.spritesheet('joinButton', 'assets/images/joinButton.png');
+    this.load.spritesheet('deleteButton', 'assets/images/deleteButton.png');
+    this.load.spritesheet('refreshButton', 'assets/images/refreshButton.png');
+    this.load.tilemap('dungeon', 'assets/tilemaps/TileMap.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('gameTiles', 'assets/tilemaps/DungeonTileSet.png');
+
     this.load.image('ship', 'assets/spaceShips_001.png');
     this.load.image('star', 'assets/star_gold.png');
 }
 
 // create sets up all functions that game.js will provide to index.js
 function create() {
-    this.addPlayer = function (self, playerInfo) {
-        const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    // ======Setting up the map=============
+
+    this.disconnectTimer = setTimeout(() => {
+        console.log("Well that escalated quickly");
+    }, 2000);
+
+    // Adds a tilemap object to the phaser game so that phaser can read from it
+    this.map = this.game.add.tilemap('dungeon');
+    // Selects the tilemap you are using.  TODO: After multiple maps are created, select one of the maps.
+    this.map.addTilesetImage('DungeonSet', 'gameTiles');
+
+    /* Sets background layer to layer called 'backgroundLayer' inside of the tilemap.
+       Should leave these the same for every map for consistency purposes. */
+    this.backgroundlayer = this.map.createLayer('backgroundLayer');
+    this.wallLayer = this.map.createLayer('wallLayer');
+
+    // Do not need as client no longer checks collision
+    // this.map.setCollisionBetween(1, 2000, true, 'wallLayer');
+
+    this.backgroundlayer.resizeWorld();
+
+    // <--------------------------------------------------->
+
+    this.addPlayer = (self, playerInfo) => {
+        let player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
         player.setDrag(100);
         player.setAngularDrag(100);
         player.setMaxVelocity(200);
